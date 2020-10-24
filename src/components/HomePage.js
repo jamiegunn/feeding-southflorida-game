bf15b47a04fb4ea7208f05496e7820648079cd42
gunn.jamie@outlook.com <gunn.jamie@outlook.com>
2020-10-24 12:38:40 -0400

    more stuff

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -11,46 +11,48 @@ import {
 class HomePage extends Component {
     render() {
         return (
-            <Container>
-                <Row>
-                    <Col md="3">
-                        <div class="border" style={{ height: '100%' }}>
-                            <CardBody>
-                                <CardTitle>Summary Running Detail</CardTitle>
-                                <Button>Button 1</Button>
-                            </CardBody>
-                        </div>
-                    </Col>
-                    <Col md="9">
-                        <div class="border" style={{ height: '100%' }}>
-                            <StepWizard nav={<CoolNav />}>
-                                <StepOne />
-                                <StepTwo />
-                            </StepWizard>
-                        </div>
-                    </Col>
-                </Row>
+            <div>
+                <div style={{ marginTop: 20 }}></div>
+                <Container>
+                    <Row>
+                        <Col md="3">
+                            <div class="border" style={{ height: '100%' }}>
+                                <CardBody>
+                                    <CardTitle>Summary Running Detail</CardTitle>
+                                    <Button>Button 1</Button>
+                                </CardBody>
+                            </div>
+                        </Col>
+                        <Col md="9">
+                            <div class="border" style={{ height: '100%' }}>
+                                <StepWizard nav={<CoolNav />}>
+                                    <StepOne />
+                                    <StepTwo />
+                                </StepWizard>
+                            </div>
+                        </Col>
+                    </Row>
 
-                <Row>
-                    <Col md="3">
-                        <div class="border" style={{ height: '100%', marginTop: 20 }}>
-                            <CardBody>
-                                <CardTitle>Family Info / Who Goes Hungry</CardTitle>
-                                <Button>Button 1</Button>
-                            </CardBody>
-                        </div>
-                    </Col>
-                    <Col md="9">
-                        <div class="border" style={{ height: '100%', marginTop: 20 }}>
-                            <CardBody>
-                                <CardTitle>Did you know?</CardTitle>
-                                <Button>Button 1</Button>
-                            </CardBody>
-                        </div>
-                    </Col>
-                </Row>
-
-            </Container>
+                    <Row>
+                        <Col md="3">
+                            <div class="border" style={{ height: '100%', marginTop: 20 }}>
+                                <CardBody>
+                                    <CardTitle>Family Info / Who Goes Hungry</CardTitle>
+                                    <Button>Button 1</Button>
+                                </CardBody>
+                            </div>
+                        </Col>
+                        <Col md="9">
+                            <div class="border" style={{ height: '100%', marginTop: 20 }}>
+                                <CardBody>
+                                    <CardTitle>Did you know?</CardTitle>
+                                    <Button>Button 1</Button>
+                                </CardBody>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         );
     }
 }

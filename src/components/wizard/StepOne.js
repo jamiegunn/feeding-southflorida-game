83ff4e4a800c7f3a03f907ac7c583b898ce778c6
gunn.jamie@outlook.com <gunn.jamie@outlook.com>
2020-10-24 11:10:40 -0400

    added product pages

--- src/components/wizard/StepOne.js
+++ src/components/wizard/StepOne.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button, Container, Row, Col
+} from 'reactstrap';
 
 class StepOne extends Component {
     render() {
         return (
+        <Container>
+            <Row>
+            <Col xs="3">
+                <Card>
+                    <CardImg width = "100%" src="https://reactstrap.github.io/assets/318x180.svg" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>Card title</CardTitle>
+                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                        <Button>Button 1</Button>&nbsp; 
+                        <Button>Button 2</Button>
+                    </CardBody>
+                </Card>
+            </Col>
+
+            <Col xs="3">
+                <Card>
+                    <CardImg width = "100%" src="https://reactstrap.github.io/assets/318x180.svg" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>Card title</CardTitle>
+                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                        <Button>Button 1</Button>&nbsp; 
+                        <Button>Button 2</Button>
+                    </CardBody>
+                </Card>
+            </Col>
+
+            <Col xs="3">
+                <Card>
+                    <CardImg width = "100%" src="https://reactstrap.github.io/assets/318x180.svg" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>Card title</CardTitle>
+                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                        <Button>Button 1</Button>&nbsp; 
+                        <Button>Button 2</Button>
+                    </CardBody>
+                </Card>
+            </Col>
+            </Row>
+
+            <Row>
+            <Col xs="3">
             <div>
-                <h2>Step {this.props.currentStep}</h2>
+ <h2>Step {this.props.currentStep}</h2>
                 <p>Total Steps: {this.props.totalSteps}</p>
                 <p>Is Active: {this.props.isActive}</p>
                 <p><Button color="danger" onClick={this.props.previousStep}>Previous Step</Button></p>
@@ -13,7 +60,10 @@ class StepOne extends Component {
                 <p><button onClick={() => this.props.goToStep(2)}>Step 2</button></p>
                 <p><button onClick={this.props.firstStep}>First Step</button></p>
                 <p><button onClick={this.props.lastStep}>Last Step</button></p>
-            </div>
+            </div>   
+            </Col>
+            </Row>
+            </Container>
         );
     }
 }
